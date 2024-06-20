f2e1b8854758314f76a2cf4b6a35860e4348febb
sandlad <thiagoareias3@gmail.com>
2024-06-19 21:15:01 -0300

    Desafio realizado

---
+++ index.js
@@ -0,0 +1,19 @@
+let mc = [ 
+    {nome: "Zummaer", xp: 5470}
+]
+for (let loop = 0; loop < mc.length; loop++)
+    {
+        let nomeMc = mc[loop].nome
+        let xpMc = mc[loop].xp
+        let nivel = ""
+    
+    if(xpMc <= 1000){nivel = "Ferro"}
+    else if(xpMc >= 1001 && xpMc <= 2000) {nivel = "Bronze"}
+    else if(xpMc >= 2001 && xpMc <= 5000) {nivel = "Prata"}
+    else if(xpMc >= 5001 && xpMc <= 7000) {nivel = "Ouro"}
+    else if(xpMc >= 7001 && xpMc <= 8000) {nivel = "Platina"}
+    else if(xpMc >= 8001 && xpMc <= 9000) {nivel = "Ascendente"}
+    else if(xpMc >= 9001 && xpMc <= 10000) {nivel = "Imortal"}
+    else if(xpMc > 10000){nivel = "Radiante"}
+        console.log("O Herói de nome " + nomeMc + " está no nível de " + nivel + ", com " + xpMc + " pontos de experiência.")
+    }
---
+++ test.js
@@ -0,0 +1,7 @@
+n = 0;
+x = 0;
+while (n < 3) {
+  n++;
+  x += n;
+}
+console.log(n)
